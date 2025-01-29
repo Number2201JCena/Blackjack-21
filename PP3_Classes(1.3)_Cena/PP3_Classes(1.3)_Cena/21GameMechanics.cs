300e7090e6d78eee7d9afa82312ef6098a5debbf
Number2201JCena <127736220+Number2201JCena@users.noreply.github.com>
2025-01-30 01:36:32 +0800

    Add files via upload

---
+++ PP3_Classes(1.3)_Cena/PP3_Classes(1.3)_Cena/21GameMechanics.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using _2Y_OOP_2324_ADeckOfCards;
+
+namespace PP3_Classes_1._3__Cena
+{
+	internal class _1GameMechanics
+	{
+        static DeckOfCards _doc = new DeckOfCards(true);
+
+        public int Introduction()
+		{
+			string Choice = "";
+			int PlayerFinalScore = 0;
+			Console.WriteLine("Welcome to a game of 21! Here, your goal is to get as close or exactly to 21" +
+				" without going past 21.");
+			Console.WriteLine("You have 2 options: Draw a card or stopping. Drawing will draw a random card from a standard 52-card" +
+				" deck while stopping will stop the game at the current state.");
+			Console.WriteLine("To begin, press D.");
+			Console.WriteLine("(D)raw or (S)top?");
+			Choice = Console.ReadLine().ToUpper();
+
+
+            if (Choice == "D")
+            {
+                PlayerFinalScore = DrawOrStop(Choice);
+            }
+            else
+            {
+                Console.WriteLine("Error Found! Please Press a Key to Try Again.");
+                Console.ReadKey();
+                Console.Clear();
+                Introduction();
+            }
+			return PlayerFinalScore;
+		}
+
+		private int DrawOrStop(string Decision)
+		{
+			string NextMove = "";
+			bool Proceed = true;
+			bool Over21 = false;
+			int CurrentPoints = 0;
+			int FinalPoints = 0;
+			while (Proceed)
+			{
+				if (Over21 == true)
+				{
+					Proceed = false;
+					break;
+				}
+
+
+                if (Decision == "D")
+                {
+					CurrentPoints = CardDrawer();
+
+					if (CurrentPoints + FinalPoints <= 21)
+					{
+						FinalPoints += CurrentPoints;
+					}
+					else
+					{
+						FinalPoints = 0;
+						Over21 = true;
+						break;
+					}
+                }
+                else if (Decision == "S")
+                {
+					break;
+                }
+
+
+				if(Over21)
+				{
+					break;
+				}
+				else
+				{
+					Console.WriteLine("Your score is " + FinalPoints + " over 21.");
+                    Console.WriteLine("The chosen move has been fully completed. What's your next move: (D)raw or (S)top?");
+                    NextMove = Console.ReadLine().ToUpper();
+                    Proceed = ContinueOrStop(NextMove);
+                }
+			}
+
+			Console.Clear();
+			Console.WriteLine("The game has been stopped.");
+		
+			return FinalPoints;
+			
+		}
+
+		private bool ContinueOrStop(string NextMove)
+		{
+			bool Continue = true;
+
+            if (NextMove == "D")
+            {
+				Continue = true;
+            }
+            else if (NextMove == "S")
+            {
+				Continue = false;
+            }
+
+			return Continue;
+        }
+
+        private int CardDrawer()
+        {
+            int CardPointValue = 0;
+            DeckOfCards doc = new DeckOfCards(true);
+            Card draw = doc.drawACard();
+            List<Card> CurrentCard = new List<Card>();
+
+
+			CurrentCard = doc.drawACard(1);
+
+			foreach(Card c in CurrentCard)
+			{
+				CardPointValue = c.GetCardValue();
+			}
+			Console.WriteLine("The card you drew had a value of: " + CardPointValue);
+            return CardPointValue;
+        }
+    }
+
+}
+
---
+++ PP3_Classes(1.3)_Cena/PP3_Classes(1.3)_Cena/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PP3_Classes_1._3__Cena;
+
+namespace _2Y_OOP_2324_ADeckOfCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            _1GameMechanics Game = new _1GameMechanics();
+            int FinalScore = 0;
+            bool Replay = true;
+            string ReplayChoice = "";
+            
+            while(Replay)
+            {
+                FinalScore = Game.Introduction();
+                Console.WriteLine("In the end, you got " + FinalScore + " over 21");
+                Console.WriteLine("Would you like to play again? Type the first letter of your choice" +
+                    " to choose. (Y)es or (N)o");
+                ReplayChoice = Console.ReadLine().ToUpper();
+
+                if(ReplayChoice == "Y")
+                {
+                    Replay = true;
+                }
+                else if(ReplayChoice == "N")
+                {
+                    Replay = false;
+                }
+                Console.Clear();
+            }
+            Console.WriteLine("Thank you for playing!");
+            Console.ReadKey();
+            
+        }
+    }
+}
